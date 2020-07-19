03c4a67b17d070ca233e97a7199a69f8b5d681a7
E003838 <azra.jarebica@bawagpsk.com>
2020-07-20 00:33:39 +0200

    Update home tab

--- src/views/home.ts
+++ src/views/home.ts
@@ -1,33 +1,36 @@
 export default (userId: string): any => ({
   type: 'home',
   blocks: [
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: '*Welcome home, <@' + userId + '> :house:*',
-      },
-    },
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text:
-          'Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>.',
-      },
-    },
-    {
-      type: 'divider',
-    },
-    {
-      type: 'context',
-      elements: [
-        {
-          type: 'mrkdwn',
-          text:
-            'Psssst this home tab was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>',
-        },
-      ],
-    },
-  ],
+		{
+			type: "section",
+			text: {
+				type: "mrkdwn",
+				text: "*Hi there <@" + userId + "> :wave: Welcome to CalBot :calendar:*"
+			}
+		},
+		{
+			type: "divider"
+		},
+		{
+			type: "section",
+			text: {
+				type: "mrkdwn",
+				text: "Great to see you here! CalBot helps you check your *<https://calendar.klika.ba/|Klika Calendar>* data right here within Slack. You will be able to check the following data using this commands: "
+			}
+		},
+		{
+			type: "section",
+			text: {
+				type: "mrkdwn",
+				text: "• */kc stats* - Check all your data aggregated \n • */kc vacation* - Check your vacation data\n • */kc religiousDays* - Check your religious days data \n • */kc wfhDays* - Check your WFH data \n • */kc help* - Get help with the commands"
+			}
+		},
+		{
+			type: "section",
+			text: {
+				type: "mrkdwn",
+				text: "Don't forget to update your calendar and enjoy using CalBot!"
+			}
+		}
+	]
 });
